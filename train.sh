a4d812a83229a1d5c7fb5867b101e3f521977076
hoangntc <hoangnguyen3892@gmail.com>
2022-07-25 12:49:15 -0400

    remove ctgcn

--- train.sh
+++ train.sh
@@ -1,7 +1,6 @@
 name=yelp
 python3 main.py --config=config/$name.json --task=preprocessing --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=VGRNN
-python3 main.py --config=config/$name.json --task=embedding --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=DynGEM
 python3 main.py --config=config/$name.json --task=embedding --method=DynAE
 python3 main.py --config=config/$name.json --task=embedding --method=DynRNN
@@ -10,7 +9,6 @@ python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN
 name=yelp_s
 python3 main.py --config=config/$name.json --task=preprocessing --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=VGRNN
-python3 main.py --config=config/$name.json --task=embedding --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=DynGEM
 python3 main.py --config=config/$name.json --task=embedding --method=DynAE
 python3 main.py --config=config/$name.json --task=embedding --method=DynRNN
@@ -19,7 +17,6 @@ python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN
 name=dblp_four_area
 python3 main.py --config=config/$name.json --task=preprocessing --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=VGRNN
-python3 main.py --config=config/$name.json --task=embedding --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=DynGEM
 python3 main.py --config=config/$name.json --task=embedding --method=DynAE
 python3 main.py --config=config/$name.json --task=embedding --method=DynRNN
@@ -28,7 +25,6 @@ python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN
 name=dblp_four_area_s
 python3 main.py --config=config/$name.json --task=preprocessing --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=VGRNN
-python3 main.py --config=config/$name.json --task=embedding --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=DynGEM
 python3 main.py --config=config/$name.json --task=embedding --method=DynAE
 python3 main.py --config=config/$name.json --task=embedding --method=DynRNN
