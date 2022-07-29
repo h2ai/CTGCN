12c3c178e1e446696a28ff4b66b7a361fb201ec4
Hoang Le <hminle@yorku.ca>
2022-07-29 18:40:57 -0400

    Update train.sh
    update

--- train.sh
+++ train.sh
@@ -5,16 +5,16 @@ python3 main.py --config=config/$name.json --task=embedding --method=VGRNN > ./l
 # python3 main.py --config=config/$name.json --task=embedding --method=DynGEM > ./logs/out_ye
 # python3 main.py --config=config/$name.json --task=embedding --method=DynAE
 python3 main.py --config=config/$name.json --task=embedding --method=DynRNN > ./logs/out_yelp_DynRNN.txt 2>&1
-python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN > ./logs/out_yelp_DynAERNN 2>&1
+# python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN > ./logs/out_yelp_DynAERNN 2>&1 --> Done
 
 echo "yelp_s"
 name=yelp_s
 # python3 main.py --config=config/$name.json --task=preprocessing --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=VGRNN > ./logs/out_yelp_s_VGRNN.txt 2>&1
 # python3 main.py --config=config/$name.json --task=embedding --method=DynGEM
-python3 main.py --config=config/$name.json --task=embedding --method=DynAE > ./logs/out_yelp_s_DynAE.txt 2>&1
+# python3 main.py --config=config/$name.json --task=embedding --method=DynAE > ./logs/out_yelp_s_DynAE.txt 2>&1 --> Done
 python3 main.py --config=config/$name.json --task=embedding --method=DynRNN > ./logs/out_yelp_s_DynRNN.txt 2>&1
-python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN  > ./logs/out_yelp_s_DynAERNN.txt 2>&1
+# python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN  > ./logs/out_yelp_s_DynAERNN.txt 2>&1 --> Done
 
 echo "dblp_four_area"
 name=dblp_four_area
@@ -30,8 +30,8 @@ name=dblp_four_area_s
 # python3 main.py --config=config/$name.json --task=preprocessing --method=CTGCN-C
 python3 main.py --config=config/$name.json --task=embedding --method=VGRNN > ./logs/out_dblp_four_area_s_VGRNN.txt 2>&1
 # python3 main.py --config=config/$name.json --task=embedding --method=DynGEM
-python3 main.py --config=config/$name.json --task=embedding --method=DynAE > ./logs/out_dblp_four_area_s_DynAE.txt 2>&1
+# python3 main.py --config=config/$name.json --task=embedding --method=DynAE > ./logs/out_dblp_four_area_s_DynAE.txt 2>&1 --> Done
 python3 main.py --config=config/$name.json --task=embedding --method=DynRNN > ./logs/out_dblp_four_area_s_DynRNN.txt 2>&1
-python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN > ./logs/out_dblp_four_area_s_DynAERNN.txt 2>&1
+# python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN > ./logs/out_dblp_four_area_s_DynAERNN.txt 2>&1 --> Done
 
 
