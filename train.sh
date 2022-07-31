39f53d88d8b77c2e0febb6fc995096d37f3f8a0c
Hoang Nguyen <hoangntc.nguyen@gmail.com>
2022-07-31 10:32:03 -0400

    train from 5

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
-echo "yelp"
-name=yelp
+# echo "yelp"
+# name=yelp
 # python3 main.py --config=config/$name.json --task=preprocessing --method=CTGCN-C
 # python3 main.py --config=config/$name.json --task=embedding --method=VGRNN > ./logs/out_yelp_VGRNN.txt 2>&1
-python3 main.py --config=config/$name.json --task=embedding --method=DynGEM > ./logs/out_yelp_DynGEM.txt 2>&1
+# python3 main.py --config=config/$name.json --task=embedding --method=DynGEM > ./logs/out_yelp_DynGEM.txt 2>&1
 # python3 main.py --config=config/$name.json --task=embedding --method=DynAE > ./logs/out_yelp_DynAE.txt 2>&1
-python3 main.py --config=config/$name.json --task=embedding --method=DynRNN > ./logs/out_yelp_DynRNN.txt 2>&1
-python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN > ./logs/out_yelp_DynAERNN 2>&1 --> Done
+# python3 main.py --config=config/$name.json --task=embedding --method=DynRNN > ./logs/out_yelp_DynRNN.txt 2>&1
+# python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN > ./logs/out_yelp_DynAERNN 2>&1 --> Done
 
 # echo "yelp_s"
 # name=yelp_s
@@ -22,7 +22,6 @@ name=dblp_four_area
 python3 main.py --config=config/$name.json --task=embedding --method=VGRNN > ./logs/out_dblp_four_area_VGRNN.txt 2>&1
 python3 main.py --config=config/$name.json --task=embedding --method=DynGEM > ./logs/out_dblp_four_area_DynGEM.txt 2>&1
 python3 main.py --config=config/$name.json --task=embedding --method=DynAE > ./logs/out_dblp_four_area_DynAE.txt 2>&1
-python3 main.py --config=config/$name.json --task=embedding --method=DynRNN > ./logs/out_dblp_four_area_DynRNN.txt 2>&1
 python3 main.py --config=config/$name.json --task=embedding --method=DynAERNN > ./logs/out_dblp_four_area_DynAERNN.txt 2>&1
 
 # echo "dblp_four_area_s"
